80684a18e1b05030e26fe6e80a4e8075c9af5057
Gonçalo <goncaloalmeida.artwork@gmail.com>
2026-03-05 15:10:36 +0000

    Criação dos exercicios de teste

---
+++ RightSize/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace RightSize
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            foreach (string s in args)
+            {
+                if (s.Length > 3)
+                {
+                    if(s.Length >= 8)
+                    {
+                        break;
+                    }
+
+                    Console.WriteLine(s);
+                }
+            }
+        }
+    }
+}
