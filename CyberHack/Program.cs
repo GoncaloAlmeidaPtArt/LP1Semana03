a363fa79619441f37744f0d75e5999b6087b9e61
Gonçalo <goncaloalmeida.artwork@gmail.com>
2026-03-05 16:01:29 +0000

    Pede e recebe a resposta do utilizador

--- CyberHack/Program.cs
+++ CyberHack/Program.cs
@@ -6,7 +6,11 @@ namespace CyberHack
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello LP!");
+            Console.WriteLine("Qual sistema deseja atacar ?");
+            string resposta1 = Console.ReadLine();
+
+            Console.WriteLine("Qual ferramente de hack queres usar ?");
+            string resposta2 = Console.ReadLine();
         }
     }
 }
